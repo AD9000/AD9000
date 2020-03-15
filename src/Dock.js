445745edd4b58bc8e3bcd68872ccecf6a0c7007e
AD9000 <damle.atharv16@gmail.com>
2020-03-15 17:46:23 +1100

    added a bit of style to the dock

--- src/Dock.js
+++ src/Dock.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Paper } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import "./Dock.css";
 
 export class Dock extends React.Component {
+  style = {
+    back: { backgroundColor: "rgb(255, 255, 255, 0.6" }
+  };
   handleGitClick = e => {
     window.open("https://github.com/AD9000");
   };
@@ -14,18 +17,25 @@ export class Dock extends React.Component {
   };
   render() {
     return (
-      <Grid className="Dock">
-        <GitHubIcon
-          onClick={this.handleGitClick}
-          style={{ fontSize: "60px" }}
-          className="DockIcon"
-        />
-        <LinkedInIcon
-          onClick={this.handleLinkedInClick}
-          style={{ fontSize: "60px" }}
-          className="DockIcon"
-        />
-      </Grid>
+      <Paper
+        elevation={3}
+        // classes={this.style.back}
+        // classes="DockBackground"
+        //  style={{ backgroundColor: "rgb(255, 255, 255, 0.6" }}
+      >
+        <Grid className="Dock">
+          <GitHubIcon
+            onClick={this.handleGitClick}
+            style={{ fontSize: "60px" }}
+            className="DockIcon"
+          />
+          <LinkedInIcon
+            onClick={this.handleLinkedInClick}
+            style={{ fontSize: "60px" }}
+            className="DockIcon"
+          />
+        </Grid>
+      </Paper>
     );
   }
 }
